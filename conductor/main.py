cb445f34b50ff9774d257b235303061a8ca76536
Giuseppe Marino <peppe98.marino@gmail.com>
2022-06-05 15:41:19 +0200

    update conduction json print

--- conductor/main.py
+++ conductor/main.py
@@ -2,8 +2,9 @@
 from regex import F
 import requests
 import random
+import json
 
-url = 'http://localhost:5002'
+url = 'http://localhost:5001'
 
 def random_data() -> str:
     subjects = ['Alice', 'Bob', 'Carl', 'Dave', 'Eve', 'Frank', 'George', 'Harry', 'Ida']
@@ -20,14 +21,15 @@ if response.status_code == 200:
 else:
     print(f'{response.status_code} - {response.reason}')
 
+# %%
 
 #post on http://localhost:5000/transactions/new
-data = {'sender': 'Alice', 'recipient': 'Bob', 'amount': '5'}
+data = {'sender': 'pippo', 'recipient': 'pluto', 'amount': '10'}
 response = requests.post(f'{url}/transactions/new', json=data)
 
 if response.status_code == 201:
     response_dict = response.json()
-    print(f'{response_dict}')
+    print(f'{json.dumps(response_dict, indent=2)}')
 else:
     print(f'{response.status_code}')
 
